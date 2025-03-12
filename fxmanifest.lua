cf581e6d1d3c8960cd8be5f9a1641fb5916c64fd
NickTacke <the.nick.tacke@gmail.com>
2025-03-12 12:44:53 +0100

    filled in fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'txApi'
+author 'Arceas'
+version '1.0.0'
+repository 'https://github.com/NickTacke/txApi'
+description 'A sketchy FiveM resource that uses txAdmin web-endpoint to access players/actions'
+
+server_only 'yes'
+server_scripts {
+    'txApi.lua'
+}
