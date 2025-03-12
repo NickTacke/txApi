0cd72fe0bef413c2d383cb81f96c4d5733962d38
NickTacke <the.nick.tacke@gmail.com>
2025-03-12 16:44:04 +0100

    Update version to 1.1.3 and include configuration file in server scripts

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,12 +4,13 @@ lua54 'yes'
 
 name 'txApi'
 author 'Arceas'
-version '1.0.0'
+version '1.1.3'
 repository 'https://github.com/NickTacke/txApi'
 description 'A sketchy FiveM resource that uses txAdmin web-endpoints to access players/actions'
 
 server_only 'yes'
 server_scripts {
+    'config.lua',
     'api.lua',
     'actions.lua',
     'players.lua',
