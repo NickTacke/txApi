58d8ec6fd90ac01390c02bbe7a5a9e3a2c1cdd04
NickTacke <the.nick.tacke@gmail.com>
2025-03-12 13:02:24 +0100

    validate parameters in api.new function; trim trailing slash from hostname

--- api.lua
+++ api.lua
@@ -4,8 +4,27 @@ _api.__index = _api
 
 function _api.new(hostname, username, password)
     local self = setmetatable({}, _api)
-    self.hostname = hostname
 
+    -- Check if required parameters are provided
+    if not hostname or not username or not password then
+        error("Hostname, username and password are required")
+    end
+
+    -- Remove trailing / from hostname
+    if hostname:sub(-1) == "/" then
+        hostname = hostname:sub(1, -2)
+    end
+    
+    -- Store variables for later use
+    self.hostname = hostname
+    self.username = username
+
+    -- Json object to send to auth endpoint
+    local loginData = json.encode({
+        username = username,
+        password = password
+    })
+    
     
     
     return self
