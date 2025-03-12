2e4661b50fcd1a485cd2641a216535bb7d8fe6f8
NickTacke <the.nick.tacke@gmail.com>
2025-03-12 16:43:54 +0100

    Add configuration file for authentication and settings

---
+++ config.lua
@@ -0,0 +1,10 @@
+-- config.lua
+Config = {}
+
+-- Authentication details
+Config.Hostname = "http://localhost:40120"
+Config.Username = "admin" -- Fill with the username you set in txAdmin
+Config.Password = "admin" -- Fill with the password you set in txAdmin
+
+-- Settings
+Config.Debug = false -- Set to true to enable debug messages
