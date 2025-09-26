013c92a18d7c29ae8ad9079249528ca197bfb9c9
NickTacke <the.nick.tacke@gmail.com>
2025-09-26 21:49:22 +0100

    Update config.lua to change the txAdmin password to a stronger value and modify the whitelisted resource from "hlrp-testing" to "txApi-testing" for improved security and clarity.

--- settings/config.lua
+++ settings/config.lua
@@ -3,13 +3,13 @@ Config = {}
 -- Authentication details
 Config.Hostname = "http://localhost:40120"
 Config.Username = "txapi" -- txAdmin username
-Config.Password = "HeLmOnD4321!" -- txAdmin password
+Config.Password = "SuperStrongPassword" -- txAdmin password
 
 -- Whitelisted resources
 Config.Whitelist = {
-    "hlrp-testing",
+    "txApi-testing",
 }
 
 -- Settings
 -- LogLevel: Set to "error", "warn", "info", "debug", or "trace"
-Config.LogLevel = "info" 
+Config.LogLevel = "info"
