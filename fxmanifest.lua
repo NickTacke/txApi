3bd2e2fdb8039aee43f75dddd3d240e78bfac957
NickTacke <the.nick.tacke@gmail.com>
2025-09-26 21:48:17 +0100

    Update version in fxmanifest.lua from 1.5.3 to 2.0.3 and maintain repository link and description.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,10 @@
-shared_script '@HaarlemSecure/shared_fg-obfuscated.lua'
-shared_script '@HaarlemSecure/ai_module_fg-obfuscated.lua'
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
 name 'txApi'
 author 'Nick'
-version '1.5.3'
+version '2.0.3'
 repository 'https://github.com/NickTacke/txApi'
 description 'FiveM resource that uses txAdmin web-endpoints to access players/actions'
 
