45eed0256f51110cb226a5d6d72e47dcfb61c7d7
aysihuniks <askinceyhun78@gmail.com>
2026-01-05 22:19:41 +0300

    fix(server): correct uptime fallback

--- modules/server.lua
+++ modules/server.lua
@@ -1,13 +1,15 @@
 txApi.server = {}
 
+local startTsMs = os.time() * 1000
+
 ---@return { ok: boolean, uptimeMs: number, uptimeSeconds: number }
 function txApi.server.uptime()
     local uptimeMs
     if type(GetGameTimer) == 'function' then
         uptimeMs = GetGameTimer()
     else
-        -- Fallback: coarse uptime since unix epoch start. Not accurate but better than nothing.
-        uptimeMs = os.time() * 1000
+        -- Fallback: coarse uptime since module init.
+        uptimeMs = (os.time() * 1000) - startTsMs
     end
     return {
         ok = true,
